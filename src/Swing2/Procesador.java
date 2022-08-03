ab0ec1e1bc3926f1af063bac8f5c1a19496c6230
Alejandro Nacimba <davidps4@hotmail.es>
2022-08-02 21:21:55 -0500

    Leer la clase Procesador e interpretar su funcionamiento

--- src/Swing2/Procesador.java
+++ src/Swing2/Procesador.java
@@ -3,7 +3,6 @@ package Swing2;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.text.*;
 
 public class Procesador {
     
@@ -33,69 +32,87 @@ class laminaProcesador extends JPanel{
         JPanel laminaMenu = new JPanel();
         JMenuBar barraMenu = new JMenuBar();
         
-        JMenu fuente = new JMenu("Fuente");
-        JMenu estilo = new JMenu("Estilo");
-        JMenu tamano = new JMenu("Tamano");
+        fuente = new JMenu("Fuente");
+        estilo = new JMenu("Estilo");
+        tamanio = new JMenu("Tamano");
 
-        JMenuItem arial = new JMenuItem("Arial");
-        JMenuItem serif = new JMenuItem("Serif");
-        JMenuItem times = new JMenuItem("Times New Roman");
-        fuente.add(arial);
-        fuente.add(serif);
-        fuente.add(times);
+        configMenu("Arial", "Fuente", "Arial", 9, 10);
+        configMenu("Times New Roman", "Fuente", "Times New Roman", 9, 10);
+        configMenu("Calibri", "Fuente", "Calibri", 9, 10);
 
-        JMenuItem negrita = new JMenuItem("Negrita");
-        JMenuItem cursiva = new JMenuItem("Cursiva");
-        estilo.add(negrita);
-        estilo.add(cursiva);
+        configMenu("Negrita", "Estilo", "", Font.BOLD, 1);
+        configMenu("Cursiva", "Estilo", "", Font.ITALIC, 1);
 
-        JMenuItem pequeno = new JMenuItem("10");
-        JMenuItem mediano = new JMenuItem("16");
-        JMenuItem grande = new JMenuItem("24");
-        tamano.add(pequeno);
-        tamano.add(mediano);
-        tamano.add(grande);
+        configMenu("12", "Tamano", "12", 9, 12);
+        configMenu("16", "Tamano", "16", 9, 16);
+        configMenu("20", "Tamano", "20", 9, 20);
+        configMenu("24", "Tamano", "24", 9, 24);
 
         barraMenu.add(fuente);
         barraMenu.add(estilo);
-        barraMenu.add(tamano);
+        barraMenu.add(tamanio);
 
         laminaMenu.add(barraMenu);
         add(laminaMenu, BorderLayout.NORTH);
 
-        espacioTexto = new JTextArea(8, 20);
-        espacioTexto.setFont(new Font("Helvetica", Font.PLAIN, 12));
+        miArea = new JTextPane();
+        add(miArea, BorderLayout.CENTER);
 
-        manejaFuente fonts = new manejaFuente();
-        
-        arial.addActionListener(fonts);
-        serif.addActionListener(fonts);
-        times.addActionListener(fonts);
+    }  
 
-        add(espacioTexto, BorderLayout.CENTER);
+    public void configMenu(String rotulo, String menu, String tipo_letra, int style, int tam){
 
+        JMenuItem elem_menu = new JMenuItem(rotulo);
+
+        if(menu == "Fuente"){
+            fuente.add(elem_menu);
+        }else if(menu == "Estilo"){
+            estilo.add(elem_menu);
+        }else if(menu == "Tamano"){
+            tamanio.add(elem_menu);
+        }
+
+        elem_menu.addActionListener(new gestionaEventos(rotulo, tipo_letra, style, tam));
     }
 
-    private class manejaFuente implements ActionListener{
+    private class gestionaEventos implements ActionListener{
+
+        String tipo_texto, menu;
+        int tamano_letra, estilo_letra;
+
+        gestionaEventos(String elemento, String texto2, int estilo2, int tam_letra){
+
+            tipo_texto = texto2;
+            estilo_letra = estilo2;
+            tamano_letra = tam_letra;
+            menu = elemento;
+        }
 
         public void actionPerformed(ActionEvent e){
 
-            if(e.getActionCommand().equalsIgnoreCase("arial")){
+            letras = miArea.getFont();
 
-                espacioTexto.setFont(new Font("Arial", Font.PLAIN, 12));
+            if(menu == "Arial" || menu == "Times New Roman" || menu == "Calibri"){
 
-            }else if(e.getSource().equalsIgnoreCase("serif")){
+                estilo_letra = letras.getStyle();
+                tamano_letra = letras.getSize();
 
-                espacioTexto.setFont(new Font("Serif", Font.PLAIN, 12));
+            }else if(menu == "Negrita" || menu == "Cursiva"){
 
-            }else if(e.getSource().equals("times")){
+                tipo_texto = letras.getFontName();
+                tamano_letra = letras.getSize();
 
-                espacioTexto.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+            }else if(menu == "12" || menu == "16" || menu == "20" || menu == "24"){
 
+                tipo_texto = letras.getFontName();
+                estilo_letra = letras.getStyle();
             }
+
+            miArea.setFont(new Font(tipo_texto, estilo_letra, tamano_letra));
         }
     }
-
-    private JTextArea espacioTexto; 
+    private JTextPane miArea;
+    private JMenu fuente, estilo, tamanio;
+    private Font letras;
 }
 
